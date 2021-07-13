fd66d6b62c9076baf990fa66c85068643aaaccfc
Julian Vega <ingjulianvega@gmail.com>
2021-07-12 21:14:04 -0500

    :sparkles: Error messages

--- src/main/java/ingjulianvega/ximic/msscasubilling/configuration/ErrorCodeMessages.java
+++ src/main/java/ingjulianvega/ximic/msscasubilling/configuration/ErrorCodeMessages.java
@@ -7,7 +7,7 @@ public class ErrorCodeMessages {
 
     public static final String BILLING_NOT_FOUND_API_CODE = "401";
     public static final String BILLING_NOT_FOUND_ERROR = "BILLING_NOT_FOUND";
-    public static final String BILLING_NOT_FOUND_MESSAGE = "No se encontró el método de administración";
+    public static final String BILLING_NOT_FOUND_MESSAGE = "No se encontró el pago";
     public static final String BILLING_NOT_FOUND_SOLUTION = "Verifique los datos";
 
     public static final String ARGUMENT_NOT_VALID_API_CODE = "402";
